228d5ff84422155d86400b98eecb392beddaa1f1
Marie Bovtramovich <m.abovt@mail.ru>
2021-04-14 00:17:36 +0300

    fix: return dispatchProps to events and gifts

--- src/components/myEvents.tsx
+++ src/components/myEvents.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { IMyEventsProps, IEventProps } from '../types';
 import { MyEvent } from './myEvent';
 
-export const Events: React.FC<IMyEventsProps> = ({ events }) => (
+export const Events: React.FC<IMyEventsProps> = ({ events, editItem, deleteItem }) => (
   <>
     {
          events.map((item: IEventProps) => (
-           <MyEvent {...item} key={item.id} />
+           <MyEvent {...item} key={item.id} editItem={editItem} deleteItem={deleteItem} />
          ))
       }
   </>
--- src/components/myGifts.tsx
+++ src/components/myGifts.tsx
@@ -2,10 +2,17 @@ import React from 'react';
 import { IMyGiftsProps, IGiftProps } from '../types';
 import { MyGift } from './myGift';
 
-export const Gifts: React.FC<IMyGiftsProps> = ({ gifts }) => (
+export const Gifts: React.FC<IMyGiftsProps> = ({ gifts, editItem, deleteItem }) => (
   <>
     {
-      gifts.map((g: IGiftProps) => <MyGift {...g} key={g.id} />)
+      gifts.map((g: IGiftProps) => (
+        <MyGift
+          {...g}
+          key={g.id}
+          editItem={editItem}
+          deleteItem={deleteItem}
+        />
+      ))
     }
   </>
 );
